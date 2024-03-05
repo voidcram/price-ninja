d0033a3d78362195d60c93af0c5055f4f512c6bf
marcosbd23 <marcosbrdr@gmail.com>
2024-03-05 16:08:07 +0000

    close #1

---
+++ Api/src/api/v1/schemas.js
@@ -0,0 +1,30 @@
+import Joi from "joi";
+
+// Check if its a url of PcComponentes
+function validateURL(value, helpers) {
+  try {
+    const parsedURL = new URL(value);
+    if (parsedURL.hostname === "www.pccomponentes.com") {
+      return value; // Return the validated URL
+    } else {
+      throw new Error("URL is not from the allowed domain");
+    }
+  } catch (error) {
+    return helpers.error("any.invalid");
+  }
+}
+
+export const updateSchema = Joi.object({
+  name: Joi.string().max(50).required(),
+  url: Joi.string().uri().custom(validateURL, 'custom validation').required(),
+  vendor: Joi.string().max(30).required(),
+  brand: Joi.string().max(30).required(),
+  stock: Joi.boolean().required(),
+  currentPrice: Joi.number().precision(2).required(),
+  originalPrice: Joi.number().precision(2).required(),
+  lowestPrice: Joi.number().precision(2).required()
+});
+
+export const scrapeSchema = Joi.object({
+  url: Joi.string().uri().custom(validateURL, 'custom validation').required(),
+});
