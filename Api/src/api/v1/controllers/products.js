2cdddb71129770d472acb1ba2070a565f26b8517
voidcram <marcosbrdr@gmail.com>
2024-03-05 03:28:02 +0000

    Added CRUD routes

--- Api/src/api/v1/controllers/products.js
+++ Api/src/api/v1/controllers/products.js
@@ -1,25 +1,69 @@
-import { Product } from '../../../models/Product.js';
+import { Change } from "../../../models/Change.js";
+import { Product } from "../../../models/Product.js";
+
+export async function scrapeProduct(req, res) {
+  // TODO: validate body input and do the scrape system
+  res.send("Scrapeando");
+}
 
 export async function getAll(req, res) {
-    try {
-        const products = await Product.findAll();
-        res.json(products);
-    } catch (error) {
-        console.error('Error fetching products:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
 export async function getById(req, res) {
-    const { id } = req.params;
-    try {
-        const product = await Product.findByPk(id);
-        if (!product) {
-            return res.status(404).json({ error: 'Product not found' });
-        }
-        res.json(product);
-    } catch (error) {
-        console.error('Error fetching product by ID:', error);
-        res.status(500).json({ error: 'Internal server error' });
+  const { id } = req.params;
+  try {
+    const product = await Product.findByPk(id);
+    if (!product) return res.status(404).json({ message: "Product not found" });
+
+    res.json(product);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export async function getChanges(req, res) {
+  const { id } = req.params;
+  try {
+    const changes = Change.findAll({ where: { id } });
+    res.json(changes);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export async function updateProduct(req, res) {
+  const { id } = req.params;
+
+  try {
+    const [updated] = await Product.update(req.body, { where: { id } });
+
+    // Return updated project incase it got updated
+    if (updated) {
+      const updatedProduct = await Product.findOne({ where: { id } });
+      return res.status(200).json(updatedProduct);
     }
+
+    return res.status(404).json({ message: "Product not found" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export async function deleteProduct(req, res) {
+  const { id } = req.params;
+
+  try {
+    const deleted = await Product.destroy({ where: { id } });
+    if (deleted) return res.status(204).send();
+
+    return res.status(404).json({ message: "Product not found" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 }
--- Api/src/api/v1/routes/products.js
+++ Api/src/api/v1/routes/products.js
@@ -4,7 +4,21 @@ import * as productsController from "../controllers/products.js";
 const router = express.Router();
 
 // Routes for products
+
+// Create / Scrape new product
+router.post('/', productsController.scrapeProduct);
+
+// Read product/s
 router.get('/', productsController.getAll);
 router.get('/:id', productsController.getById);
+
+// Get product changes i do it separated cause overtime it gonna scale to many data
+router.get('/:id/changes', productsController.getChanges);
+
+// Update product
+router.put('/:id', productsController.updateProduct)
+
+// Delete product
+router.delete('/:id', productsController.deleteProduct)
 
 export default router;
