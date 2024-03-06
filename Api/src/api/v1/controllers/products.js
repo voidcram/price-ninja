04218f86b8433cca9e3c0d3e08ab8139f984d766
marcosbd23 <marcosbrdr@gmail.com>
2024-03-06 19:28:36 +0000

    Added new routes for PATCH and GET

--- Api/src/api/v1/controllers/products.js
+++ Api/src/api/v1/controllers/products.js
@@ -1,19 +1,38 @@
 import { Change } from "../../../models/Change.js";
 import { Product } from "../../../models/Product.js";
-import { updateSchema, scrapeSchema } from "../schemas.js";
+import { Category } from "../../../models/Category.js";
+import { productSchema, patchSchema } from "../schemas/schemas.js";
 
-export async function scrapeProduct(req, res) {
+export async function createProduct(req, res) {
+  const productData = req.body;
+
   try {
-    const validation = scrapeSchema.validate(req.body);
-
+    const validation = productSchema.validate(req.body);
     if (validation.error) {
       return res.status(400).json(validation.error.details);
     }
 
-    const newProduct = await Product.create(req.body);
+    // Check if product with that url already exists
+    let exists = await Product.findOne({ where: { url: productData.url}})
+    if (exists) {
+      return res.status(409).json({ message: "Product already exists"})
+    }
+
+    // Check if the category exists
+    let category = await Category.findOne({ where: { name: productData.category } });
+    if (!category) {
+      return res.status(404).json({ message: "Category doesnt exist"})
+    }
+
+    // Add the category ID to the product data, and change the key to category_id 
+    productData.category_id = category.id;
+    delete productData.category;
+
+    const newProduct = await Product.create(productData);
 
     return res.status(201).json(newProduct);
   } catch (error) {
+    res.log.error(`Error creating product: ${error}`)
     return res.status(500).json({ message: "Internal Server Error" });
   }
 }
@@ -23,6 +42,7 @@ export async function getAll(req, res) {
     const products = await Product.findAll();
     res.json(products);
   } catch (error) {
+    res.log.error(`Error getting all products: ${error}`)
     return res.status(500).json({ message: "Internal Server Error" });
   }
 }
@@ -35,14 +55,29 @@ export async function getById(req, res) {
 
     res.json(product);
   } catch (error) {
+    res.log.error(`Error product by id ${id}: ${error}`)
     return res.status(500).json({ message: "Internal Server Error" });
   }
 }
 
-export async function getChanges(req, res) {
-  const { id } = req.params;
+// TODO
+export async function getByCategory(req, res) {
+  const { category_id } = req.params;
   try {
-    const changes = Change.findAll({ where: { id } });
+    const product = await Product.findByPk(category_id);
+    if (!product) return res.status(404).json({ message: "Product not found" });
+
+    res.json(product);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+// TODO change this using foreign key now
+export async function getChanges(req, res) {
+  const { product_id } = req.params;
+  try {
+    const changes = Change.findAll({ where: { product_id } });
     res.json(changes);
   } catch (error) {
     return res.status(500).json({ message: "Internal Server Error" });
@@ -51,17 +86,22 @@ export async function getChanges(req, res) {
 
 export async function updateProduct(req, res) {
   const { id } = req.params;
+  const productData = req.body;
 
   try {
-    const validation = updateSchema.validate(req.body);
-
+    const validation = productSchema.validate(productData);
     if (validation.error) {
       return res.status(400).json(validation.error.details);
     }
 
-    const [updated] = await Product.update(req.body, { where: { id } });
+    let category = await Category.findOne({ where: { name: productData.category } });
+    if (!category) {
+      return res.status(404).json({ message: "Category doesnt exist"})
+    }
 
-    // Return updated project incase it got updated
+    const [updated] = await Product.update(productData, { where: { id } });
+
+    // Return updated product incase it got updated
     if (updated) {
       const updatedProduct = await Product.findOne({ where: { id } });
       return res.status(200).json(updatedProduct);
@@ -69,6 +109,34 @@ export async function updateProduct(req, res) {
 
     return res.status(404).json({ message: "Product not found" });
   } catch (error) {
+    res.log.error(`Error updating product with id ${id}: ${error}`)
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export async function patchProduct(req, res) {
+  const { id } = req.params;
+
+  try {
+    const validation = patchSchema.validate(req.body);
+    if (validation.error) {
+      return res.status(400).json(validation.error.details);
+    }
+
+    // Retrieve the product by ID
+    const product = await Product.findByPk(id);
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    // Apply updates to the product
+    Object.assign(product, req.body);
+    await product.save();
+
+    // Respond with the updated product
+    return res.status(200).json(product);
+  } catch (error) {
+    res.log.error(`Error patching product with id ${id}: ${error}`)
     return res.status(500).json({ message: "Internal Server Error" });
   }
 }
@@ -82,6 +150,7 @@ export async function deleteProduct(req, res) {
 
     return res.status(404).json({ message: "Product not found" });
   } catch (error) {
+    res.log.error(`Error deleting product with id ${id}: ${error}`)
     return res.status(500).json({ message: "Internal Server Error" });
   }
 }
