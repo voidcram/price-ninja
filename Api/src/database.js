1eb43efda81e62c486d680edd919945d355f3eff
marcosbd23 <marcosbrdr@gmail.com>
2024-03-06 18:57:58 +0000

    Disable SQL logging on production

--- Api/src/database.js
+++ Api/src/database.js
@@ -4,5 +4,6 @@ import 'dotenv/config';
 export const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "mysql",
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  logging: process.env.NODE_ENV == "development" ? console.log : false
 });
