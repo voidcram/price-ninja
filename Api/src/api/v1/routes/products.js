e55b0b09fba7e0e37d7627c65f58c763ea5a4057
voidcram <marcosbrdr@gmail.com>
2024-03-06 19:28:36 +0000

    Added new routes for PATCH and GET

--- Api/src/api/v1/routes/products.js
+++ Api/src/api/v1/routes/products.js
@@ -5,18 +5,24 @@ const router = express.Router();
 
 // Routes for products
 
-// Create / Scrape new product
-router.post('/', productsController.scrapeProduct);
+// Create new product
+router.post('/', productsController.createProduct);
 
 // Read product/s
 router.get('/', productsController.getAll);
 router.get('/:id', productsController.getById);
 
-// Get product changes i do it separated cause overtime it gonna scale to many data
+// Get products by category
+router.get('/category/:id', productsController.getByCategory);
+
+// Get product changes (i do it separated cause overtime it gonna scale to many changes)
 router.get('/:id/changes', productsController.getChanges);
 
 // Update product
 router.put('/:id', productsController.updateProduct)
+
+// Update specific product fields
+router.patch('/:id', productsController.patchProduct)
 
 // Delete product
 router.delete('/:id', productsController.deleteProduct)
