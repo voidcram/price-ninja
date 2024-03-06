6540ddbcbf44c300f0998ecdc2074f8322c25e87
voidcram <marcosbrdr@gmail.com>
2024-03-06 19:39:03 +0000

    homepage almost done

--- nextjs-page/src/app/page.tsx
+++ nextjs-page/src/app/page.tsx
@@ -1,7 +1,31 @@
-const Homepage = () => {
-  return (
-    <div>página de inicio</div>
-  )
+import styles from './home.module.css';
+import { Button } from "@/components/ui/button";
+import Image from 'next/image';
+
+const Home = () => {
+  return <div className={styles.container}>
+    <div className={styles.textContainer}>
+      <h1 className={styles.title}>Price Ninja</h1>
+      <p className={styles.desc}>
+      En Price Ninja, nos apasiona la tecnología y queremos 
+      ayudarte a encontrar los componentes perfectos para
+      potenciar tu experiencia informática. Ya sea que estés 
+      construyendo tu propio PC o buscando actualizaciones, 
+      aquí encontrarás la información más precisa y las 
+      comparaciones más detalladas.
+      </p>
+      <div className={styles.buttonGroup}>
+        <Button>Sabes más</Button>
+        <Button variant='outline'>Contacto</Button>
+      </div>
+      <div className={styles.brands}>
+        <Image src='/brands.png' alt='' fill className={styles.brandImg}/>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src='/lobo-ninja-bien.png' alt='' fill className={styles.loboImg}/>
+      </div>
+    </div>
+  </div>
 }
 
-export default Homepage
+export default Home
