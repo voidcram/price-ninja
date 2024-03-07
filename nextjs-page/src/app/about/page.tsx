48354d4f779721d469437c32f5107ef52cdade17
adexe03 <adexe.reyes.cb@gmail.com>
2024-03-07 16:58:43 +0000

    starting about us page

--- nextjs-page/src/app/about/page.tsx
+++ nextjs-page/src/app/about/page.tsx
@@ -1,6 +1,13 @@
+import styles from "./about.module.css"
+
 const AboutPage = () => {
   return (
-    <div>Sobre nosotros</div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2>Sobre Price Ninja</h2>
+      </div>
+      <div className={styles.imgContainer}></div>
+    </div>
   )
 }
 
