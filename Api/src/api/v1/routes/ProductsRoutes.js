f21e74a23258197749b34fd38a9417b6538610eb
marcosbd23 <marcosbrdr@gmail.com>
2024-03-07 04:02:24 +0000

    Files renames

--- Api/src/api/v1/routes/ProductsRoutes.js
+++ Api/src/api/v1/routes/ProductsRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as productsController from "../controllers/products.js";
+import * as productsController from "../controllers/ProductsController.js";
 
 const router = express.Router();
 
