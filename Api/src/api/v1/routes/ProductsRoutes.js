36a2075ab1944d3c532d11b5cd60e6aa3b92b0fd
voidcram <marcosbrdr@gmail.com>
2024-03-10 19:54:21 +0000

    Improved logger and added cache close #5

--- Api/src/api/v1/routes/ProductsRoutes.js
+++ Api/src/api/v1/routes/ProductsRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import paginate from 'express-paginate';
 import * as productsController from "../controllers/ProductsController.js";
+import apicache from 'apicache-plus'
 
 const router = express.Router();
 
@@ -11,14 +12,14 @@ router.post('/', productsController.createProduct);
 router.post('/scrape', productsController.scrapeProduct);
 
 // Get product/s
-router.get('/', paginate.middleware(10, 50), productsController.getAll);
-router.get('/:id', productsController.getById);
+router.get('/', paginate.middleware(10, 50), apicache('1 hour'), productsController.getAll);
+router.get('/:id', apicache('1 hour'), productsController.getById);
 
 // Get products by category
-router.get('/categories/:id', paginate.middleware(10, 50), productsController.getByCategory);
+router.get('/categories/:id', paginate.middleware(10, 50), apicache('1 hour'), productsController.getByCategory);
 
 // Get product changes (i do it separated cause overtime it gonna scale to many changes)
-router.get('/:id/changes', paginate.middleware(10, 50), productsController.getChanges);
+router.get('/:id/changes', paginate.middleware(10, 50), apicache('1 hour'), productsController.getChanges);
 
 // Update product
 router.put('/:id', productsController.updateProduct)
--- Api/src/config/database.js
+++ Api/src/config/database.js
@@ -1,5 +1,6 @@
 import Sequelize from "sequelize";
 import 'dotenv/config';
+import logger from "./logger.js"
 
 export const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
@@ -14,9 +15,9 @@ async function syncDatabase() {
       // Uncomment for reset all tables
       // await sequelize.sync({force: true});
       await sequelize.sync();
-      console.log('Database synchronized successfully');
+      logger.info(`Database synchronized successfully`)
   } catch (error) {
-      console.error('Unable to sync database:', error);
+      logger.error(error, "Unable to sync database");
       process.exit(1);
   }
 }
