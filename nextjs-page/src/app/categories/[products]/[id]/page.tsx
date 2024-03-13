513b236a9194f3e02ade72f3843b4a037bc9a9c1
adexe03 <adexe.reyes.cb@gmail.com>
2024-03-13 17:06:06 +0000

    end product view page

--- nextjs-page/src/app/categories/[products]/[id]/page.tsx
+++ nextjs-page/src/app/categories/[products]/[id]/page.tsx
@@ -1,6 +1,25 @@
+import * as React from "react"
+
 const ProductPage = () => {
     return (
-        <div>Producto en cuestion</div>
+        <div className="container mx-auto">
+            <h1>Página de visualización de producto</h1>
+            <div className="flex flex-col md:flex-row">
+                <div className="md:w-1/2">
+                    <img src="https://via.placeholder.com/600x400" alt="Imagen del producto" />
+                </div>
+                <div className="md:w-1/2 p-4">
+                    <h2>Nombre del producto</h2>
+                    <p>Descripción del producto</p>
+                    <ul>
+                        <li>Característica 1</li>
+                        <li>Característica 2</li>
+                        <li>Característica 3</li>
+                    </ul>
+                    <button className="bg-blue-500 text-white p-2 rounded">Comprar</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
