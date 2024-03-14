bf9022029ef27b84f56af4fb9ef29aad7831152c
marcosbd23 <marcosbrdr@gmail.com>
2024-03-14 21:51:09 +0000

    .env example

--- nextjs-page/next.config.mjs
+++ nextjs-page/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["thumb.pccomponentes.com"],
+  },
+};
 
 export default nextConfig;
